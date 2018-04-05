2220e2e27265e044d58caa68dfa558aa0675626f
Rocky Chiang <rocky@chem.ucla.edu>
2018-04-05 05:46:35 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,13 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
   get "/reversename/:name" do 
     @name = params[:name].reverse
     "#{@name}"
   end
   
   get "/square/:number" do
-    
+    @num = params[:number].to_i * params[:number].to_i
   end
 
 end
