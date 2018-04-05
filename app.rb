97214f594257f81a9d1b9cf7fc470c04193c69df
Rocky Chiang <rocky@chem.ucla.edu>
2018-04-05 05:56:12 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -28,9 +28,10 @@ class App < Sinatra::Base
   end
   
   get "/:operation/:number1/:number2" do 
+    @num1 = params[:number1]
+    @num2 = params[:number2]
     case params[:operation]
     when "add"
-      @num1 = params[:number1]
-      @num2 = params[:number2]
+      "#{@num3}"
   end
 end
