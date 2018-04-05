e6dbd3cb6aa65bf05373a6d822082d4b711cef98
Rocky Chiang <rocky@chem.ucla.edu>
2018-04-05 06:05:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ class App < Sinatra::Base
     params[:number].to_i.times do
       @sentence << @phrase
     end
-    
+    "#{@sentence.join(" ")}"
   end
   
   get "/say/:word1/:word2/:word3/:word4/:word5" do
